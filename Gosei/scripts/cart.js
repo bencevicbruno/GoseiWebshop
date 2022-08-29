84d36cd082fe07f85586b6e2fa4209232b209224
karlo81121 <adzic.karlo74@gmail.com>
2022-08-29 16:50:29 +0200

    new commit

---
+++ Gosei/scripts/cart.js
@@ -0,0 +1,5 @@
+const emptyCartMessage = document.querySelector('.empty-cart');
+emptyCartMessage.style.display = 'none';
+
+const lineBetweenProducts = document.querySelector('.line-between');
+lineBetweenProducts.style.display = 'none';
